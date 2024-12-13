0d84533995198023b3a4627f06cc076fc5705a1d
ryutaKimu <rekkoilove@softbank.ne.jp>
2024-12-14 00:00:27 +0900

    first commit

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Midnight Crescendo</title>
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header>
+    <h1>Midnight Crescendo</h1>
+    <time>2024年12月13日</time>
+    <p>元旦を熱い気持ちで迎えましょう！</p>
+    <p>こちらのサービスはタイムゾーンに対応しています。下記からタイムゾーンを選択してください。</p>
+  </header>
+  <main>
+    <p>下記よりタイムゾーンを選択してください</p>
+    <select name="timezone" id="timezone">
+      <option>日本</option>
+      <option>アメリカ</option>
+      <option>イギリス</option>
+    </select>
+  </main>
+  <footer>
+    <p>&copy; 2024 Midnight Crescendo. All rights reserved.</p>
+  </footer>
+</body>
+</html>
